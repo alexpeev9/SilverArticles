e08286ab24d54ec1d4ec7b7d81d754d50107660f
alexpeev9 <alexpeev9@abv.bg>
2023-02-01 22:14:09 +0200

    Temporary Remove Build

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -5,10 +5,12 @@ const router = Router();
 
 router.use('/api', (req: any, res: any) => res.json({ status: 200 }));
 
-router.use('*', (req: any, res: any) => {
-  return res.sendFile(
-    path.join(__dirname, '../../../', '/client/build/index.html')
-  );
-});
+router.use('/*', (req: any, res: any) => res.json({ status: 200 }));
+
+// router.use('*', (req: any, res: any) => {
+//   return res.sendFile(
+//     path.join(__dirname, '../../../', '/client/build/index.html')
+//   );
+// });
 
 export default router;
