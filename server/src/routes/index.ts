a0305542276d5aa37d5ae69f7365e703d82bb6aa
alexpeev9 <alexpeev9@abv.bg>
2023-02-01 23:58:23 +0200

    Check if Connected to Db

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+import mongoose from 'mongoose';
 
 const router = Router();
 
-router.use('/api', (req: any, res: any) => res.json({ status: 200 }));
+router.use('/api', (req: any, res: any) =>
+  res.json({ status: mongoose.connection.readyState })
+);
 
 router.use('*', (req: any, res: any) => res.json({ status: 200 }));
 
