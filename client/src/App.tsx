dd6228ed01c3b9535df9fdc665c0bc847fba5bd3
alexpeev9 <alexpeev9@abv.bg>
2023-02-02 16:38:54 +0200

    Add Test Commits

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+
 import './App.css';
 
-function App() {
+const findUser = async () => {
+  return new Promise<string>((resolve, reject) => {
+    fetch(`${process.env.REACT_APP_API_PROD_URL}/find`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin':
+          process.env.REACT_APP_API_PROD_URL || 'http://localhost:5000'
+      },
+      credentials: 'include'
+    })
+      .then((data) => data.json())
+      .then((data) => resolve(data))
+      .catch((e) => reject({ status: e.status, statusCode: e.statusCode }));
+  });
+};
+
+const App = () => {
+  const [user, setUser] = useState('Pesho');
+  console.log(process.env.REACT_APP_API_PROD_URL);
+  useEffect(() => {
+    const getUser = async () => {
+      const data = await findUser();
+      console.log(data);
+      setUser(data);
+    };
+    getUser();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{user}</p>
       </header>
     </div>
   );
-}
+};
 
 export default App;
