8cdcffd9918a678635a05554281becba27c57241
alexpeev9 <alexpeev9@abv.bg>
2023-05-10 00:51:16 +0300

    Reduce middleware number

--- server/src/middlewares/authMiddleware.ts
+++ server/src/middlewares/authMiddleware.ts
@@ -1,9 +1,10 @@
 import { Request, Response, NextFunction } from 'express'
 import jwt from 'jsonwebtoken'
 
+import articleService from '../services/articleService'
 import { jwtSecret } from '../env'
 
-const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+const loggedMiddleware = (req: Request, res: Response, next: NextFunction) => {
   try {
     const token = req.cookies['token']
     if (token) {
@@ -18,4 +19,30 @@ const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
   }
 }
 
-export default authMiddleware
+const accessMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { slug } = req.params
+    const { reqToken } = req.body
+
+    const currentUser = { username: reqToken.username, roleId: reqToken.roleId }
+    const article = await articleService.getOne(slug)
+
+    // check if the requester of the info is the author or administrator
+    if (!articleService.checkIfCreator(article, currentUser)) {
+      throw new Error('Article Not Found!')
+    }
+
+    req.body.article = article
+    next()
+  } catch (err: any) {
+    return res.status(403).json({
+      errors: [`${err.message}`]
+    })
+  }
+}
+
+export { loggedMiddleware, accessMiddleware }
--- server/src/routes/articleRoutes.ts
+++ server/src/routes/articleRoutes.ts
@@ -1,19 +1,21 @@
 import { Router } from 'express'
 
 import articleController from '../controllers/articleController'
-import authMiddleware from '../middlewares/authMiddleware'
-import { accessMiddleware } from '../middlewares/articleMiddleware'
+import {
+  accessMiddleware,
+  loggedMiddleware
+} from '../middlewares/authMiddleware'
 
 const router = Router()
 
 router.route('/').get(articleController.getAll)
-router.route('/create').post(authMiddleware, articleController.create)
+router.route('/create').post(loggedMiddleware, articleController.create)
 router
   .route('/update/:slug')
-  .put(authMiddleware, accessMiddleware, articleController.update)
+  .put(loggedMiddleware, accessMiddleware, articleController.update)
 router
   .route('/remove/:slug')
-  .delete(authMiddleware, accessMiddleware, articleController.remove)
+  .delete(loggedMiddleware, accessMiddleware, articleController.remove)
 router.route('/get/:number/:order').get(articleController.getXNumber)
 router.route('/:slug').get(articleController.getOne)
 
