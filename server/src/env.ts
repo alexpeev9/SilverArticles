25d45f62be0466d28518ba70ffc3f99a2ede8f78
alexpeev9 <62713012+alexpeev9@users.noreply.github.com>
2023-05-28 04:37:54 +0300

    Update env.ts

--- server/src/env.ts
+++ server/src/env.ts
@@ -9,11 +9,11 @@ export const connectionString = isInProduction
     'mongodb+srv://{{user}}:{{password}}@{{cluster}}/{{database-name}}'
   : 'mongodb://0.0.0.0:27017/silver-articles'
 export const clientUrl = isInProduction
-  ? `${process.env.CLIENT_URL}` || 'https://your-react-site.com'
+  ? `${process.env.CLIENT_URL || 'https://your-react-site.com'}`
   : 'http://localhost:3000'
-export const apiPort = process.env.API_PORT || 5000
-export const adminPwd = process.env.ADMIN_PWD || 'admin'
-export const salt = `${process.env.SALT}` || 9
+export const apiPort = `${process.env.API_PORT || 5000}`
+export const adminPwd = `${process.env.ADMIN_PWD || 'admin'}`
+export const salt = `${process.env.SALT || 9}`
 export const jwtSecret =
   `${process.env.JWT_SECRET}` || '2625B2735C5D6A8C325424CA34912'
 export const roleIds = {
